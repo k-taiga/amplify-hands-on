b2bc98fb2f8b1389c5806fe37c55dd069d3007e2
K-taiga <taigakatou0329@gmail.com>
2025-02-15 19:21:35 +0900

    [update] change authentication method to Amazon Cognito and adjust related configurations

--- amplifyapp/src/App.js
+++ amplifyapp/src/App.js
@@ -5,7 +5,7 @@ import "@aws-amplify/ui-react/styles.css";
 import { withAuthenticator } from "@aws-amplify/ui-react";
 import { Amplify } from "aws-amplify";
 
-import aws_exports from "./aws-exports";
+import aws_exports from "./aws-exports.js";
 import { DynamoDB } from "./components/DynamoDB";
 
 Amplify.configure(aws_exports);
