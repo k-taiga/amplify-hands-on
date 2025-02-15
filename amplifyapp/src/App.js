859546cf46e72e64efb1beae57299b13252db785
K-taiga <taigakatou0329@gmail.com>
2025-02-15 19:46:39 +0900

    [update] remove Amplify configuration from App.js and clean up imports

--- amplifyapp/src/App.js
+++ amplifyapp/src/App.js
@@ -3,12 +3,8 @@ import "./App.css";
 import "@aws-amplify/ui-react/styles.css";
 
 import { withAuthenticator } from "@aws-amplify/ui-react";
-import { Amplify } from "aws-amplify";
 
-import aws_exports from "./aws-exports.js";
 import { DynamoDB } from "./components/DynamoDB";
-
-Amplify.configure(aws_exports);
 
 function App() {
   return (
